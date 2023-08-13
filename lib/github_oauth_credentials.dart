4d874706c4072e1e4e0ea3fdf6d83fc315ab51d6
Zingo21 <82944718+Zingo21@users.noreply.github.com>
2023-08-13 20:05:00 +0200

    rename github_oauth_cedentials.dart to github_oauth_credentials.dart

---
+++ lib/github_oauth_credentials.dart
@@ -0,0 +1,6 @@
+// TODO(CodelabUser): Create an Oauth App
+const githubClientId = '075cd02d99cff4fbb210';
+const githubClientSecret = 'd56a112afc1bc5fdea146980534f24b17f924413';
+
+// Oauth scopes for repository and user information
+const githubScopes = ['repo', 'read:org'];
